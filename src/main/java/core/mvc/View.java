8241fff5ae9ff7732923b7ba9e6697d7bbaf2ff8
hyunseo <wndlthsk@naver.com>
2025-07-30 23:52:07 +0900

    refactor: view 인터페이스 추가

---
+++ src/main/java/core/mvc/View.java
@@ -0,0 +1,9 @@
+package core.mvc;
+
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface View {
+    void render(HttpServletRequest request, HttpServletResponse response) throws Exception;
+}
