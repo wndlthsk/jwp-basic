848c5c94be68f144bbe029b76d00114f01e8f50a
hyunseo <wndlthsk@naver.com>
2025-07-30 21:26:14 +0900

    refactor: findAll 구현을 jdbctemplate으로 리팩토링

--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -36,35 +36,22 @@ public class UserDao {
     }
 
     public List<User> findAll() throws SQLException {
-        // TODO 구현 필요함.
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            con = ConnectionManager.getConnection();
-            String sql = "SELECT userId, password, name, email FROM USERS";
-            pstmt = con.prepareStatement(sql);
+        RowMapper<User> rm = new RowMapper<User>() {
+            @Override
+            public User mapRow(ResultSet rs) throws SQLException {
+                return new User(
+                    rs.getString("userId"),
+                    rs.getString("password"),
+                    rs.getString("name"),
+                    rs.getString("email")
+                );
+            }
+        };
 
-            rs = pstmt.executeQuery();
+        JdbcTemplate template = new JdbcTemplate();
 
-            List<User> users = new ArrayList<>();
-            if (rs.next()) {
-                users.add(new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
-                    rs.getString("email")));
-            }
-
-            return users;
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
-            }
-        }
+        String sql = "SELECT userId, password, name, email FROM USERS";
+        return template.list(sql, rm);
     }
 
     public User findByUserId(String userId) throws SQLException {
--- src/main/java/next/support/JdbcTemplate.java
+++ src/main/java/next/support/JdbcTemplate.java
@@ -5,6 +5,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JdbcTemplate {
     public void executeUpdate(String sql, PreparedStatementSetter pss) throws SQLException {
@@ -32,6 +34,18 @@ public class JdbcTemplate {
     }
 
     public <T> T executeQuery(String sql, RowMapper<T> rm, PreparedStatementSetter pss) throws SQLException {
+        List<T> list = list(sql, rm, pss);
+        if (list.isEmpty()) {
+            return null;
+        }
+        return list.get(0);
+    }
+
+    public <T> T executeQuery(String sql, RowMapper<T> rm, Object... parameters) throws SQLException {
+        return executeQuery(sql, rm, createPreparedStatementSetter(parameters));
+    }
+
+    public <T> List<T> list(String sql, RowMapper<T> rm, PreparedStatementSetter pss) throws SQLException {
         Connection con = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
@@ -41,11 +55,13 @@ public class JdbcTemplate {
             pss.setParameters(pstmt);
 
             rs = pstmt.executeQuery();
-            if (!rs.next()) {
-                return null;
+
+            List<T> list = new ArrayList<T>();
+            while (rs.next()) {
+                list.add(rm.mapRow(rs));
             }
 
-            return rm.mapRow(rs);
+            return list;
         } finally {
             if (rs != null) {
                 rs.close();
@@ -59,8 +75,8 @@ public class JdbcTemplate {
         }
     }
 
-    public <T> T executeQuery(String sql, RowMapper<T> rm, Object... parameters) throws SQLException {
-        return executeQuery(sql, rm, createPreparedStatementSetter(parameters));
+    public <T> List<T> list(String sql, RowMapper<T> rm, Object... parameters) throws SQLException {
+        return list(sql, rm, createPreparedStatementSetter(parameters));
     }
 
     private static PreparedStatementSetter createPreparedStatementSetter(Object... parameters) {
