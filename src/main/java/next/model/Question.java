f88acc2e38a9f690f2a409caacb1a487e6a04e18
hyunseo <wndlthsk@naver.com>
2025-08-02 23:52:50 +0900

    fix: (8) 답변 추가시 질문 테이블의 countOfAnswer 증가 로직 추가 및 countOfAnswer로 필드명 통일

--- src/main/java/next/model/Question.java
+++ src/main/java/next/model/Question.java
@@ -13,20 +13,20 @@ public class Question {
 
     private Date createdDate;
 
-    private int countOfComment;
+    private int countOfAnswer;
 
     public Question(String writer, String title, String contents) {
         this(0, writer, title, contents, new Date(), 0);
     }
 
     public Question(long questionId, String writer, String title, String contents, Date createdDate,
-            int countOfComment) {
+            int countOfAnswer) {
         this.questionId = questionId;
         this.writer = writer;
         this.title = title;
         this.contents = contents;
         this.createdDate = createdDate;
-        this.countOfComment = countOfComment;
+        this.countOfAnswer = countOfAnswer;
     }
 
     public long getQuestionId() {
@@ -53,14 +53,14 @@ public class Question {
         return this.createdDate.getTime();
     }
 
-    public int getCountOfComment() {
-        return countOfComment;
+    public int getCountOfAnswer() {
+        return countOfAnswer;
     }
 
     @Override
     public String toString() {
         return "Question [questionId=" + questionId + ", writer=" + writer + ", title=" + title + ", contents="
-                + contents + ", createdDate=" + createdDate + ", countOfComment=" + countOfComment + "]";
+                + contents + ", createdDate=" + createdDate + ", countOfAnswer=" + countOfAnswer + "]";
     }
 
     @Override
