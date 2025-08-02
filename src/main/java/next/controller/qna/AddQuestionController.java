26d5a7f2491233a108ee181251975ec4b4d1a338
hyunseo <wndlthsk@naver.com>
2025-08-02 22:06:49 +0900

    feat: (3,4) 로그인 사용자 정보를 사용한 질문 추가 기능 구현

---
+++ src/main/java/next/controller/qna/AddQuestionController.java
@@ -0,0 +1,34 @@
+package next.controller.qna;
+
+import core.mvc.AbstractController;
+import core.mvc.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import next.controller.UserSessionUtils;
+import next.dao.AnswerDao;
+import next.dao.QuestionDao;
+import next.model.Answer;
+import next.model.Question;
+import next.model.User;
+
+public class AddQuestionController extends AbstractController {
+    private QuestionDao questionDao = new QuestionDao();
+
+    @Override
+    public ModelAndView execute(HttpServletRequest req, HttpServletResponse response) throws Exception {
+        if (!UserSessionUtils.isLogined(req.getSession())) {
+            return jspView("redirect:/users/loginForm");
+        }
+
+        User user = UserSessionUtils.getUserFromSession(req.getSession());
+
+        Question question = new Question(
+            user.getUserId(),
+            req.getParameter("title"),
+            req.getParameter("contents")
+        );
+        questionDao.insert(question);
+
+        return jspView("redirect:/");
+    }
+}
