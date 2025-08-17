8e9af5b953a901d1a6d16e6826ef9b7eb0632f33
hyunseo <wndlthsk@naver.com>
2025-08-17 13:09:44 +0900

    feat: 자바 리플렉션 예제

--- src/test/java/core/ref/Junit3TestRunner.java
+++ src/test/java/core/ref/Junit3TestRunner.java
@@ -1,11 +1,18 @@
 package core.ref;
 
+
+import java.lang.reflect.Method;
 import org.junit.Test;
 
 public class Junit3TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method method : methods) {
+            if (method.getName().startsWith("test")) {
+                method.invoke(clazz.newInstance());
+            }
+        }
     }
 }
--- src/test/java/core/ref/ReflectionTest.java
+++ src/test/java/core/ref/ReflectionTest.java
@@ -1,5 +1,8 @@
 package core.ref;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,17 +17,47 @@ public class ReflectionTest {
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            logger.debug(String.valueOf(field));
+        }
+
+        Constructor<?>[] constructors = clazz.getDeclaredConstructors();
+        for (Constructor<?> c : constructors) {
+            logger.debug(String.valueOf(c));
+        }
+
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method m : methods) {
+            logger.debug(String.valueOf(m));
+        }
     }
     
     @Test
-    public void newInstanceWithConstructorArgs() {
+    public void newInstanceWithConstructorArgs() throws Exception {
         Class<User> clazz = User.class;
         logger.debug(clazz.getName());
+
+        Constructor<?>[] constructors = clazz.getDeclaredConstructors();
+        User user = (User) constructors[0].newInstance("userId", "password", "name", "email");
+        logger.debug(user.toString());
     }
     
     @Test
-    public void privateFieldAccess() {
+    public void privateFieldAccess() throws Exception {
         Class<Student> clazz = Student.class;
         logger.debug(clazz.getName());
+
+        Student student = new Student();
+        Field name = clazz.getDeclaredField("name");
+        name.setAccessible(true);
+        name.set(student, "주한");
+
+        Field age = clazz.getDeclaredField("age");
+        age.setAccessible(true);
+        age.setInt(student, 14);
+
+        logger.debug("Name: {}, Age: {}", student.getName(), student.getAge());
     }
 }
